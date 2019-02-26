0e33081735a4c772f0699173c5004c8e0385a5e8
dut <dustinbrickwood204@gmail.com>
2019-02-26 16:49:38 -0500

    fixed mongo string

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 require('./routes/routes')(app);
 
-const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/data';
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING_DOCKER || 'mongodb://localhost:27017/data';
 
 s.schedulerFactory.start();
 
